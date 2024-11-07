90788ea0b301069f6e3ebf62ffbb7c657af08c7e
redoprogrammer <redoprogrammer@gmail.com>
2024-11-07 23:29:52 +0700

    load dữ liệu thống kê của phần ôn tập và show chi tiết lên modal trong chức năng xem chi tiết bài thi

--- app/views/admin/statistic/review.php
+++ app/views/admin/statistic/review.php
@@ -23,16 +23,43 @@
     <table class="table table-bordered table-striped table-hover mt-3">
         <thead>
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">Môn học</th>
-                <th scope="col">Bài học</th>
-                <th scope="col">Số câu hỏi</th>
-                <th scope="col">Số user làm bài</th>
-                <th scope="col">Số lần thi</th>
+                <th rowspan="2" class="align-middle">STT</th>
+                <th rowspan="2" class="align-middle">Mã thành viên</th>
+                <th rowspan="2" class="align-middle">Tài khoản</th>
+                <th rowspan="2" class="align-middle">Họ tên</th>
+                <th colspan="3" class="text-center">Chỗ trống</th>
+                <th colspan="3" class="text-center">Câu hỏi</th>
+                <th rowspan="2"></th>
+            </tr>
+            <tr>
+                <th>Tổng số</th>
+                <th>Điền đúng</th>
+                <th>Tỉ lệ (%)</th>
+                <th>Tổng số</th>
+                <th>Trả lời đúng</th>
+                <th>Tỉ lệ (%)</th>
             </tr>
         </thead>
         <tbody id="tblData"></tbody>
     </table>
+</div>
+
+
+<div class="modal" tabindex="-1" id="modal">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Chi tiết bài thi</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" style="max-height:550px; overflow-y:auto">
+                <div class="containter p-5" id="modalContent"></div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
 </div>
 
 
@@ -41,6 +68,8 @@
         $slLessions = $('#slLessions'),
         $keyword = $('#txtKeyword'),
         $search = $('#btnSearch'),
+        $modal = $('#modal'),
+        $content = $('#modalContent'),
         $table = $('#tblData'),
         pageSize = 10;
 
@@ -49,6 +78,7 @@
         LoadSubjects();
     })
     $slLessions.change(function() {
+        $table.empty();
         if ($(this).val()) {
             LoadStatistic($(this).val());
         }
@@ -56,6 +86,7 @@
     })
 
     function LoadStatistic(lessionId) {
+        $table.empty();
         if (lessionId !== undefined) { // kiểm tra biến lessionId đã có giá trị chưa
             $.ajax({
                 url: `<?php echo BASE_URL; ?>/admin/statistic/get_review_statistic`,
@@ -68,7 +99,36 @@
                     pageSize
                 },
                 success: function(response) {
-                    console.log(response);
+                    const {
+                        code,
+                        msg,
+                        result
+                    } = response;
+                    if (code == 200) {
+                        console.log(result);
+
+                        let idx = (page - 1) * pageSize;
+                        result.forEach(l => {
+                            $table.append(`
+                            <tr>
+                                <td>${++idx}</td>
+                                <td class = "fw-bold text-warning">${l.user_code}</td>
+                                <td class = "text-info">${l.username}</td>
+                                <td>${l.fullname}</td>
+                                <td>${l.total_blanks}</td>
+                                <td>${l.correct_blanks}</td>
+                                <td>${l.correct_blanks_percent}</td>
+                                <td>${l.total_questions}</td>
+                                <td>${l.max_correct_questions}</td>
+                                <td>${l.highest_score_percentage}</td>
+                                 <td>
+                                    <a href="#" onclick="fetchDetail(${l.result_id}); return false;"><i class="fa fa-info-circle" aria-hidden="true"></i></a>
+                                </td>
+                            </tr>
+                        `);
+                        })
+
+                    }
                 },
                 error: function(err) {
                     console.log(err);
@@ -103,7 +163,7 @@
 
     // Xử lý khi chọn môn học
     $slSubjects.change(function() {
-        Loadlessions(parseInt($slSubjects.val()));       
+        Loadlessions(parseInt($slSubjects.val()));
     });
 
     // Tải danh sách bài học dựa trên môn học đã chọn
@@ -126,5 +186,97 @@
                 console.log(err);
             }
         });
+    }
+
+    function fetchDetail(id) {
+        $.ajax({
+            url: '<?php echo BASE_URL; ?>/history/detail',
+            type: 'get',
+            dataType: 'json',
+            data: {
+                id
+            },
+            success: function(response) {
+                console.log(response);
+                const {
+                    code,
+                    msg,
+                    data
+                } = response;
+                renderExamDetails(data);
+            },
+            error: function(err) {
+                console.log(err);
+            }
+        })
+    }
+
+    function renderExamDetails(data) {
+        let idx = 1;
+        $content.empty();
+        data.forEach(function(item) {
+            var questionText = item.question_text;
+            var answers = item.answers;
+
+            // Sort answers by position to ensure correct replacement order
+            answers.sort(function(a, b) {
+                return a.position - b.position;
+            });
+
+            answers.forEach(function(answer) {
+                var userAnswer = answer.user_answer;
+                var correctAnswer = answer.correct_answer;
+
+                // Tạo input cho user answer với class và style thích hợp
+                var inputClass = userAnswer === correctAnswer ? 'text-success' : 'text-danger';
+                var inputStyle =
+                    'border: none; border-bottom: 1px solid #000;'; // Không có border nhưng có gạch chân
+                var replacement = '';
+                if (userAnswer.trim().length == 0) {
+                    console.log(1);
+
+                    replacement =
+                        `<label><span class = "blank fw-bold bg-danger text-secondary" style="${inputStyle}">&nbsp;&nbsp;&nbsp;</span></label>`;
+                } else {
+                    replacement =
+                        `<input type="text" class="blank ${inputClass} fw-bold" value="${userAnswer}" style="${inputStyle}" readonly />`;
+                }
+
+
+                console.log({
+                    userAnswer,
+                    correctAnswer
+                });
+
+                if (userAnswer !== correctAnswer) {
+                    replacement +=
+                        ` [<span class="text-success fw-bold">${formatDisplay(correctAnswer.replace(/</g, '&lt;').replace(/>/g, '&gt;'))}</span>]`;
+                    console.log(correctAnswer);
+                }
+
+                // Thay thế chuỗi ___ bằng giá trị của replacement
+                var regex = /___/;
+                questionText = questionText.replace(regex, replacement);
+            });
+
+            // Append the formatted question to the container
+            var questionHtml = `<div class="row mb-1">
+                                <div class="col-md-12 form-group">
+                                    <label class="fw-bold">Câu thứ ${idx++}:</label>
+                                    <div class="mt-2 p-3">${formatDisplay(questionText)}</div>
+                                </div>
+                            </div>`;
+            $content.append(questionHtml);
+        });
+        $modal.modal('show');
+    }
+
+    const formatDisplay = function(inputHTML) {
+
+        return inputHTML
+            .replace(/\t/g, '&nbsp;&nbsp;&nbsp;&nbsp;') // Thay thế tab bằng không gian trắng
+            .replace(/<p>\s+/g, function(match) {
+                return '<p>' + '&nbsp;'.repeat(match.length - 3); // Thay thế khoảng trắng đầu dòng sau <p>
+            });
     }
 </script>
